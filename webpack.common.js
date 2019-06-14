788545625e92785012c7888496b2ca84e604f04f
Vlad <vlad@ayeps.com>
2019-06-14 09:14:17 +0300

    remove dotenv

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
 
 const pathsToClean = [
   'dist/css',
