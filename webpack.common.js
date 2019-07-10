e732db4709f4368f6eaababfdeeba154197b05c1
Vlad <vlad@ayeps.com>
2019-07-10 21:04:13 +0300

    update webpack conf

--- webpack.common.js
+++ webpack.common.js
@@ -1,197 +1,197 @@
 const dotenv = require('dotenv');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 dotenv.config();
 
-const path = require('path');
 const plugins = require('./config/common-plugins');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const sharpAdapter = require('./bin/sharp-adapter');
 
 const { PUBLIC_PATH, IMAGE_PATH } = process.env;
 
-module.exports = (env, argv) => {
-  return {
-    entry: {
-      app: './src/index.js',
+module.exports = (env, argv) => ({
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: './js/[name].[hash].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+      fonts: path.resolve(__dirname, './src/fonts'),
+      images: path.resolve(__dirname, './src/images'),
+      '~': path.join(__dirname, 'src'),
     },
-    output: {
-      filename: './js/[name].[hash].js',
-    },
-    resolve: {
-      extensions: ['.ts', '.js', '.json'],
-      alias: {
-        fonts: path.resolve(__dirname, './src/fonts'),
-        images: path.resolve(__dirname, './src/images'),
-        '~': path.join(__dirname, 'src'),
+  },
+  devServer: {
+    port: 8000,
+    contentBase: path.resolve('dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!(dom7|ssr-window|swiper)\/).*/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+          {
+            loader: 'eslint-loader',
+            options: {
+              emitErrors: true,
+            },
+          },
+        ],
       },
-    },
-    devServer: {
-      port: 8000,
-      contentBase: path.resolve('dist'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules\/(?!(dom7|ssr-window|swiper)\/).*/,
+      {
+        test: /\.pug$/,
+        use: [
+          'pug-loader',
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+          {
+            loader: 'tslint-loader',
+            options: {
+              emitErrors: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: false,
+              interpolate: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          publicPath: PUBLIC_PATH,
           use: [
             {
-              loader: 'babel-loader',
-            },
-            {
-              loader: 'eslint-loader',
+              loader: 'css-loader',
               options: {
-                emitErrors: true,
+                sourceMap: true,
               },
             },
-          ],
-        },
-        {
-          test: /\.pug$/,
-          use: [
-            'pug-loader',
-          ],
-        },
-        {
-          test: /\.tsx?$/,
-          use: [
             {
-              loader: 'ts-loader',
-            },
-            {
-              loader: 'tslint-loader',
+              loader: 'postcss-loader',
               options: {
-                emitErrors: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.html$/,
-          use: [
-            {
-              loader: 'html-loader',
-              options: {
-                minimize: false,
-                interpolate: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            publicPath: PUBLIC_PATH,
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  sourceMap: true,
-                },
-              },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  sourceMap: true,
-                  config: {
-                    ctx: {
-                      env: argv.mode,
-                    },
+                sourceMap: true,
+                config: {
+                  ctx: {
+                    env: argv.mode,
                   },
                 },
               },
-            ],
-          }),
-        },
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            publicPath: PUBLIC_PATH,
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  sourceMap: true,
-                },
+            },
+          ],
+        }),
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          publicPath: PUBLIC_PATH,
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
               },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  sourceMap: true,
-                  config: {
-                    ctx: {
-                      env: argv.mode,
-                    },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                config: {
+                  ctx: {
+                    env: argv.mode,
                   },
                 },
               },
-              {
-                loader: 'sass-loader',
-                options: {
-                  sourceMap: true,
-                },
-              },
-            ],
-          }),
-        },
-        {
-          test: /\.svg$/,
-          include: [
-            path.resolve(__dirname, 'src/images/svg-sprite'),
-          ],
-          use: [
-            {
-              loader: 'svg-sprite-loader',
             },
             {
-              loader: 'image-webpack-loader',
+              loader: 'sass-loader',
               options: {
-                svgo: {},
+                sourceMap: true,
               },
             },
           ],
-        },
-        {
-          test: /\.(eot|svg|ttf|otf|woff|woff2)$/,
-          exclude: [
-            path.resolve(__dirname, 'src/images'),
-          ],
-          use: [{
-            loader: 'url-loader',
+        }),
+      },
+      {
+        test: /\.svg$/,
+        include: [
+          path.resolve(__dirname, 'src/images/svg-sprite'),
+        ],
+        use: [
+          {
+            loader: 'svg-sprite-loader',
+          },
+          {
+            loader: 'image-webpack-loader',
             options: {
-              limit: 1000, // Convert images < 1kb to base64 strings
-              name: 'fonts/[hash]-[name].[ext]',
+              svgo: {},
             },
-          }],
-        },
-        {
-          test: /\.(jpe?g|png)$/i,
-          use: [
-            {
-              loader: 'responsive-loader',
-              options: {
-                adapter: require('./bin/sharp-adapter'),
-                outputPath: IMAGE_PATH,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(png|jp(e*)g|svg|gif)$/,
-          exclude: [
-            path.resolve(__dirname, 'src/images/svg-sprite'),
-            path.resolve(__dirname, 'src/fonts'),
-          ],
-          use: [{
-            loader: 'url-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(eot|svg|ttf|otf|woff|woff2)$/,
+        exclude: [
+          path.resolve(__dirname, 'src/images'),
+        ],
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 1000, // Convert images < 1kb to base64 strings
+            name: 'fonts/[hash]-[name].[ext]',
+          },
+        }],
+      },
+      {
+        test: /\.(jpe?g|png)$/i,
+        use: [
+          {
+            loader: 'responsive-loader',
             options: {
-              limit: 1000, // Convert images < 1kb to base64 strings
-              name: `${IMAGE_PATH}[hash].[ext]`,
+              adapter: sharpAdapter,
+              outputPath: IMAGE_PATH,
             },
-          }],
-        },
-      ],
-    },
-    plugins,
-  };
-};
+          },
+        ],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        exclude: [
+          path.resolve(__dirname, 'src/images/svg-sprite'),
+          path.resolve(__dirname, 'src/fonts'),
+        ],
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 1000, // Convert images < 1kb to base64 strings
+            name: `${IMAGE_PATH}[hash].[ext]`,
+          },
+        }],
+      },
+    ],
+  },
+  plugins,
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,9 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const plugins = require('./config/dev-plugins');
 
-module.exports = (env, argv) => {
-  return merge(common(env, argv), {
-    mode: 'development',
-    watch: true,
-    devtool: 'cheap-module-eval-source-map',
-    plugins,
-  });
-};
+module.exports = (env, argv) => merge(common(env, argv), {
+  mode: 'development',
+  watch: true,
+  devtool: 'cheap-module-eval-source-map',
+  plugins,
+});
