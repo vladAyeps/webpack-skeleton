0a41afe73133f635d367ede8a156763ef4b5e15e
Vlad <vlad@ayeps.com>
2019-07-10 20:58:09 +0300

    webpack conf update

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,18 @@
+const TerserPlugin = require('terser-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const TerserPlugin = require('terser-webpack-plugin');
 const plugins = require('./config/production-plugins');
 
-module.exports = (env, argv) => {
-  return merge(common(env, argv), {
-    mode: 'production',
-    optimization: {
-      minimizer: [
-        new TerserPlugin({
-          test: /\.js(\?.*)?$/i,
-          parallel: true,
-          sourceMap: true,
-        }),
-      ],
-    },
-    plugins,
-  });
-};
+module.exports = (env, argv) => merge(common(env, argv), {
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        parallel: true,
+        sourceMap: true,
+      }),
+    ],
+  },
+  plugins,
+});
