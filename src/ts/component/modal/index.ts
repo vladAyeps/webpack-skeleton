195278e123b445ed51bb54a260ebde4aac8bb539
Vlad <vlad@ayeps.com>
2020-08-05 21:47:48 +0400

    refactor: modal

--- src/ts/component/modal/index.ts
+++ src/ts/component/modal/index.ts
@@ -13,10 +13,12 @@ export default class Modal {
 
   constructor(opts = {}) {
     this.options = Object.assign({
-      overlaySelector: '.overlay',
-      overlayVisibilityClass: '.is-visible',
-      modalVisibilityClass: '.is-visible',
-      modalClass: '.modal',
+      overlayClass: 'overlay',
+      overlayVisibilityClass: 'is-visible',
+      modalVisibilityClass: 'is-visible',
+      modalClass: 'modal',
+      modalInClass: 'in',
+      modalOutClass: 'out',
     }, opts);
     this.closeEvent = new Event(MODAL_CLOSE_EVENT);
     this.init();
@@ -24,14 +26,16 @@ export default class Modal {
 
   private init = () => {
     this.overlay = document.createElement('div');
-    this.overlay.classList.add(this.getOption('overlaySelector'));
+    this.overlay.classList.add(this.options.overlayClass);
     this.body = document.querySelector('body');
     this.body.appendChild(this.overlay);
     this.overlay.onclick = () => {
       this.closeModal();
     };
-    forEachElement(`.${this.getOption('modalClass')}`, (el: HTMLElement) => {
+    forEachElement(`.${this.options.modalClass}`, (el: HTMLElement) => {
       this.recalculate(el);
+      el.classList.remove(this.options.modalInClass);
+      el.classList.remove(this.options.modalOutClass);
       el.style.display = 'none';
       el.style.zIndex = '-1';
     });
@@ -44,34 +48,27 @@ export default class Modal {
       return;
     }
     this.showOverlay();
+    modalWin.classList.add(this.options.modalInClass);
     modalWin.style.removeProperty('display');
     modalWin.style.removeProperty('z-index');
     const h = modalWin.offsetHeight;
+    
+    modalWin.addEventListener('transitionend', this.removeHelperClass, false);
     if (h < window.innerHeight) {
       modalWin.style.top = `${this.calculateTop(h, window.innerHeight)}px`;
     } else {
       modalWin.style.top = '0';
     }
     this.body.style.overflow = 'hidden';
-    modalWin.classList.add(this.getOption('modalVisibilityClass'));
-  };
-
-  private getOption = id => {
-    if (!this.options.hasOwnProperty(id)) {
-      return null;
-    }
-    if (typeof this.options[id] === 'string') {
-      return (<string> this.options[id]).replace(/^\./, '');
-    }
-    return this.options[id];
+    modalWin.classList.add(this.options.modalVisibilityClass);
   };
 
   private showOverlay = (): void => {
-    this.overlay.classList.add(this.getOption('overlayVisibilityClass'));
+    this.overlay.classList.add(this.options.overlayVisibilityClass);
   };
 
   public closeModal = (): void => {
-    this.overlay.classList.remove(this.getOption('overlayVisibilityClass'));
+    this.overlay.classList.remove(this.options.overlayVisibilityClass);
     this.hideActive();
     this.body.style.removeProperty('overflow');
   };
@@ -87,24 +84,39 @@ export default class Modal {
     }
   };
 
-  private getActive = (): HTMLElement => (document.querySelector(`.${this.getOption('modalClass')}.${this.getOption('modalVisibilityClass')}`));
+  private getActive = (): HTMLElement => (document.querySelector(`.${this.options.modalClass}.${this.options.modalVisibilityClass}`));
 
   private hideActive = (): void => {
     const activeModal = this.getActive();
     if (activeModal !== null) {
-      activeModal.classList.remove(this.getOption('modalVisibilityClass'));
+      activeModal.classList.remove(this.options.modalInClass);      
+      activeModal.classList.add(this.options.modalOutClass);
+      activeModal.classList.remove(this.options.modalVisibilityClass);
       activeModal.addEventListener('transitionend', this.hideModal, false);
+      activeModal.addEventListener('transitionend', this.removeHelperClass, false);
     }
   };
 
   private hideModal = (e: Event) => {
     const { target } = e;
-    if (!(<HTMLElement> target).classList.contains(this.getOption('modalClass'))) {
+    if (!(<HTMLElement> target).classList.contains(this.options.modalClass)) {
       return;
     }
     (<HTMLElement> target).style.display = 'none';
     (<HTMLElement> target).style.zIndex = '-1';
     (<HTMLElement> target).dispatchEvent(this.closeEvent);
-    target.removeEventListener('transitionend', this.hideModal, false);
+    
+    target.removeEventListener('transitionend', this.hideModal, false);    
+  };
+
+  private removeHelperClass = (e: Event) => {
+    const { target } = e;
+    if (!(<HTMLElement> target).classList.contains(this.options.modalClass)) {
+      return;
+    }
+    (<HTMLElement> target).classList.remove(this.options.modalOutClass);
+    (<HTMLElement> target).classList.remove(this.options.modalInClass);
+
+    target.removeEventListener('transitionend', this.removeHelperClass, false);
   };
 }
